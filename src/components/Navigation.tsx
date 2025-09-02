f16c5f9ab8ea0e7eb407e34770c09f22d15e25d6
Geetika Gehlot <geetikagehlot2009@gmail.com>
2025-09-01 20:06:06 -0400

    f

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -33,7 +33,7 @@ const Navigation = ({ isDarkBackground = false }: NavigationProps) => {
         className="fixed top-4 left-14 z-50 text-5xl md:text-6xl font-bebas tracking-tighter"
       >
         <span className="logo-zion"><b>ZION</b></span>
-        <span className={`logo-axelle ${logoAxelleClass} ml-1.5`}><b>AXELLE</b></span>
+        <span className={`logo-axelle ${logoAxelleClass} ml-2`}><b>AXELLE</b></span>
       </Link>
 
       {/* Horizontal Navigation - Hidden on mobile */}
