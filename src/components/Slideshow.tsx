e9af00f82da086954ea03458d750bab9dd6870d0
Geetika Gehlot <geetikagehlot2009@gmail.com>
2025-09-13 15:10:47 -0400

    darklight

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -11,46 +11,31 @@ const artworks = [
   {
     id: 1,
     image: homeartwork1,
-    title: 'Crimson Expression',
-    year: '2024',
-    medium: 'Acrylic on Canvas',
-    dimensions: '120 x 90 cm',
-    isDark: true
+    title: 'The Revolution Cannot be Contained',
+    isDark: false
   },
   {
     id: 2,
     image: homeartwork2,
-    title: 'Ocean Dreams',
-    year: '2024',
-    medium: 'Oil on Canvas',
-    dimensions: '150 x 100 cm',
+    title: 'Objectifying Women, Objectifying Bodies, Objectifying Things',
     isDark: true
   },
   {
     id: 3,
     image: homeartwork3,
-    title: 'Minimalist Harmony',
-    year: '2023',
-    medium: 'Mixed Media',
-    dimensions: '100 x 100 cm',
+    title: 'Mula sa Dagat Hanggang sa Ilog, ang Palestina ay Lalaya',
     isDark: false
   },
   {
     id: 4,
     image: homeartwork4,
-    title: 'Midnight Thoughts',
-    year: '2024',
-    medium: 'Acrylic on Canvas',
-    dimensions: '140 x 110 cm',
+    title: 'Mayari: Diyosa ng Buwan',
     isDark: true
   },
   {
     id: 5,
     image: homeartwork5,
-    title: 'Solar Burst',
-    year: '2023',
-    medium: 'Oil on Canvas',
-    dimensions: '130 x 95 cm',
+    title: 'Self Portrait #3',
     isDark: false
   },
 ];
