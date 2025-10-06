c1c19255df5eb5e3fb1c34eecfc4a3cc60eeba0b
Geetika Gehlot <geetikagehlot2009@gmail.com>
2025-10-05 22:07:39 -0400

    scale

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -98,7 +98,7 @@ const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
   src={images[currentImageIndex]}
   alt={description.split('\n')[0]}
   className="object-contain max-h-[90vh] max-w-full cursor-zoom-out transition-transform duration-100 "
-  style={{ transform: `scale(1.5) translate(${mousePosition.x}px, ${mousePosition.y}px)` }}
+  style={{ transform: `scale(2.0)) translate(${mousePosition.x}px, ${mousePosition.y}px)` }}
   onClick={() => handleImageClick()}
   draggable={false}
 />
