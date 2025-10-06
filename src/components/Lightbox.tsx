5e302e159d3d704372d5de19cf64cb10802e2531
Geetika Gehlot <geetikagehlot2009@gmail.com>
2025-10-05 22:31:39 -0400

    sfs

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -125,7 +125,7 @@ const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
                       key={idx}
                       src={img}
                       alt={`${description.split('\n')[0]} - ${idx + 1}`}
-                      className="object-contain max-h-[80vh] max-w-[50%] cursor-zoom-in transition-opacity hover:opacity-100"
+                      className="object-contain max-h-[90vh] max-w-[90%] cursor-zoom-in transition-opacity hover:opacity-100"
                       onClick={() => handleImageClick(idx)}
                       draggable={false}
                     />
