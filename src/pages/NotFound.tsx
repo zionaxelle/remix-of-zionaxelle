cdd4f179fda5c8d1f6932b66ad6ad298bf3f38a7
Geetika Gehlot <geetikagehlot2009@gmail.com>
2025-11-18 20:13:02 -0500

    gj

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -27,7 +27,7 @@ const NotFound = () => {
 
 <footer className="py-4 text-center border-t border-border bg-gray-100">
   <div className="text-xs text-muted-foreground flex flex-wrap justify-center gap-6">
-<<<<<<< HEAD
+
     <a href="https://geetikagehlot.com/" className="hover:text-artist-red transition-colors underline">
       Developed by Geetika Gehlot
 
