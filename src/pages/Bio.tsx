b7a0d8819f21fef785b8d97bcb2b4d49529b22ea
zionaxelle <zionaxellefabro@gmail.com>
2026-03-29 15:09:14 -0400

    copyright

--- src/pages/Bio.tsx
+++ src/pages/Bio.tsx
@@ -201,7 +201,7 @@ const Bio = () => {
 <footer className="py-4 text-center border-t border-border bg-gray-100">
   <div className="text-xs text-muted-foreground flex flex-wrap justify-center gap-6">
     <a href="/geetika" className="hover:text-artist-red transition-colors underline">
-      Developed by Geetika Gehlot
+      Copyright © 2026 Zion Axelle. All rights reserved. Developed by Geetika Gehlsot
     </a>
 
     <a
